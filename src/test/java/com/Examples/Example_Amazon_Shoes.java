41c36cd0dd675144ae64dd6ea40e847076430ace
Adrk51 <durakahmet049@gmail.com>
2022-06-27 00:05:27 +0200

    verifyTitle

--- src/test/java/com/Examples/Example_Amazon_Shoes.java
+++ src/test/java/com/Examples/Example_Amazon_Shoes.java
@@ -12,7 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.List;
 
 public class Example_Amazon_Shoes {
---
+++ src/test/java/com/cydeo/Day9/SingletonPractice.java
@@ -0,0 +1,10 @@
+package com.cydeo.Day9;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+    @Test
+    public void singleton_understanding_test1(){
+
+    }
+}
